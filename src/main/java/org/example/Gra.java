f094d1cf773fa56ccdc8e7dc893f46d6e3fc069b
unknown <foksinskkacper@gmai.com>
2025-11-29 19:10:14 +0100

    Pierwsyz commit

---
+++ src/main/java/org/example/Gra.java
@@ -0,0 +1,66 @@
+package org.example;
+
+public class Gra {
+    String runda;
+
+    public Gra(String runda) {
+        this.runda = runda;
+    }
+
+    public static char[] znaki(){
+        return new char[]{'A', 'Y'};
+    }
+    public char[] zwrocTabliceZnakow(){
+        return runda.toCharArray();
+    }
+    public static char zmienZnaki(char znak){
+        switch (znak){
+            case 'A':
+            case 'x':{
+                return 'K';
+            }
+            case 'B':
+            case 'Y':{
+                return 'P';
+            }
+            case 'C':
+            case 'Z':{
+                return 'N';
+            }
+        }
+        return 0;
+    }
+    public static char[] zmienZnakiWTablice(char[] znaki){
+        for (int i = 0; i < znaki.length ; i++) {
+            znaki[i] = zmienZnaki(znaki[i]);
+        }
+        return znaki;
+    }
+    public static int obliczWynik(char[] znaki){
+        String pryjorytety = "KPZ";
+        switch (znaki[0]){
+            case 'K':
+                return znaki[1] == 'P' ? 0:6;
+
+            case 'P':
+                return znaki[1] == 'N' ?6:0;
+            case 'N':
+                return znaki[1] == 'K' ?6:0;
+
+        }
+        return 3;
+
+    }
+    public static int obliczWynikDlaZnakow(char[] znaki, int wynik){
+        switch (znaki[1]){
+            case 'K':
+                return wynik+1;
+            case 'P':
+                return wynik+2;
+            case 'N':
+                return wynik+3;
+        }
+        return wynik;
+
+    }
+}
---
+++ src/test/java/Testy.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.example.Gra;
+public class Testy  {
+    @Test
+    public void porownanie(){
+    Gra gra = new Gra("AY");
+    Assertions.assertArrayEquals(new char[]{'A','Y'}, gra.zwrocTabliceZnakow());
+    }
+    @Test
+    public void zmienznaki(){
+        Assertions.assertArrayEquals(new char[]{'K','P'}, Gra.zmienZnakiWTablice(Gra.znaki()));
+    }
+    @Test
+    public void obliczWynik(){
+        Assertions.assertEquals(0, Gra.obliczWynik(Gra.zmienZnakiWTablice(Gra.znaki())));
+    }
+    @Test
+    public void obliczWynikDlaZnakow(){
+        char[] gra= {'A', 'Y'};
+        Assertions.assertEquals(3, Gra.obliczWynikDlaZnakow(Gra.znaki(),Gra.obliczWynik(gra)));
+    }
+}
